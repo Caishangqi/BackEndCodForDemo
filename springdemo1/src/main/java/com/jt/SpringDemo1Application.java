1fff6af932f6762762dd2ca3066330cc93feab9c
Caishangqi <2470159730@qq.com>
2021-11-23 23:13:04 -0800

    修改SpringDemo的拼写

--- springdemo1/src/main/java/com/jt/SpringDemo1Application.java
+++ springdemo1/src/main/java/com/jt/SpringDemo1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springdemo1Application {
+public class SpringDemo1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springdemo1Application.class, args);
+        SpringApplication.run(SpringDemo1Application.class, args);
     }
 
 }
--- springdemo1/src/test/java/com/jt/SpringDemo1ApplicationTests.java
+++ springdemo1/src/test/java/com/jt/SpringDemo1ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Springdemo1ApplicationTests {
+class SpringDemo1ApplicationTests {
 
     @Test
     void contextLoads() {
