1867430128a5549283cd55640b00c676c68423fd
Caishangqi <2470159730@qq.com>
2021-11-24 20:00:48 -0800

    对反射的解释，证明Spring创建对象使用的是反射

--- springdemo2_ioc/src/main/java/com/jt/User.java
+++ springdemo2_ioc/src/main/java/com/jt/User.java
@@ -1,6 +1,13 @@
 package com.jt;
 
+
 public class User {
+
+    //反射方法创建对象时，必然调用对象的无参构造!
+    public User() {
+        System.out.println("User的无参构造");
+    }
+
     public void say() {
         System.out.println("Is user");
     }
