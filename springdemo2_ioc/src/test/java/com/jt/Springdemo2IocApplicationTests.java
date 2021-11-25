f76533cc3bff6cb8a0cdbc2dd7692ae1eae32d0a
Caishangqi <2470159730@qq.com>
2021-11-24 18:48:02 -0800

    Spring的核心1 IOC 控制反转案例

---
+++ springdemo2_ioc/src/test/java/com/jt/Springdemo2IocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springdemo2IocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
