7497fe031a766581b278cb756617b1ac8d8bd0f5
Caishangqi <2470159730@qq.com>
2021-12-16 00:37:59 -0800

    实现图片上传 + 数据回显

--- jT/src/main/java/com/jt/service/FileServiceImpl.java
+++ jT/src/main/java/com/jt/service/FileServiceImpl.java
@@ -6,9 +6,15 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Service
 public class FileServiceImpl implements FileService {
+    //定义文件的根目录
+    private String rootDir = "H:\\JavaDevelop\\BackEndCode\\jT\\images";
+
     /**
      * 思路:
      * 1.校验文件是否为图片  校验图片类型
@@ -36,12 +42,42 @@ public class FileServiceImpl implements FileService {
             if (width == 0 || height == 0) {
                 return null;
             }
+
+
+            /**
+             * 分目录存储
+             */
+
+            String datePath = new SimpleDateFormat("/yyyy/MM/dd/")
+                    .format(new Date());
+
+            String fileDir = rootDir + datePath;
+            File dirFile = new File(fileDir);
+            if (!dirFile.exists()) {
+                dirFile.mkdirs();
+            }
+
+            //动态生成UUID
+            String UUID = java.util.UUID.randomUUID().toString();
+            String fileType = filename.substring(filename.lastIndexOf("."));
+            filename = UUID + fileType;
+
+            //实现文件的上传 (准备文件的全路径)
+            String path = fileDir + filename;
+            file.transferTo(new File(path));
+
+            //准备ImageVO数据返回
+            String virtualPath = datePath + filename;
+            String urlPath = "https://user-images.githubusercontent.com/39553613/143213875-5428c7eb-34d2-4c16-9f15-1d6400b2fea2.png";
+            ImageVO imageVO = new ImageVO(virtualPath, urlPath, filename);
+
+            return imageVO;
         } catch (Exception e) {
             //一般条件下为了不影响代码结构,将检查异常,转化为运行时异常
             e.printStackTrace();
             throw new RuntimeException(e);
         }
 
-        return null;
+
     }
 }
